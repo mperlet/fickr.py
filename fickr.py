9e219349aec9ef14339bf0ce28b42e936090be44
Mathias Perlet <mathias.perlet@igd-r.fraunhofer.de>
2014-05-26 11:03:30 +0200

    Added flickrit

--- fickr.py
+++ fickr.py
@@ -1,8 +1,10 @@
 from pyquery import PyQuery as pq
 import sys, os, re, unicodedata
 import urllib, threading, os.path
+import json
 
 url = u'http://www.flickr.com/photos/'
+flickrit_url = u'http://f.pboehm.org/photos/'
 
 def slugify(value):
     value = unicodedata.normalize('NFKD', value)
@@ -26,7 +28,7 @@ def get_name_and_quality():
             size = sys.argv[2]     # Picture-Size (o->Original, q->150x150, k->2048x???)
         except:
             size = u'o'
-    print(u'%s%s' % (url,username))
+    #print(u'%s%s' % (url,username))
     return [username, size]
 
 def find_links(url, username, size):
@@ -36,14 +38,31 @@ def find_links(url, username, size):
         photo_title = u'%s' % doc('meta[name=title]').attr("content")
         link = doc('#allsizes-photo').children().attr('src')
         print photo_title
-        path = slugify(u'%s'%username)
-        filename = u'%s/%s.jpg' % (path, slugify(photo_title))
-        if not os.path.exists(path):
-            os.mkdir(path)
-        if not os.path.isfile(filename):
-            download_thread = threading.Thread(target=download_pic, args=[link, filename])
-            download_thread.start()
+        prepare_download(username,link, photo_title)
+
+def prepare_download(username, photo_url, photo_title):
+    path = slugify(u'%s' % username)
+    filename = u'%s/%s.jpg' % (path, slugify(photo_title))
+    if not os.path.exists(path):
+        os.mkdir(path)
+    if not os.path.isfile(filename):
+        download_thread = threading.Thread(target=download_pic, args=[photo_url, filename])
+        download_thread.start()
+
+def get_links_flickit(username):
+    response = urllib.urlopen('%s%s' % (flickrit_url, username))
+    data = json.load(response)
+    for photo in data:
+        print photo['title']
+        prepare_download(username,photo['url_o'], photo['title'])
+
+
 
 # Execute
 username, size = get_name_and_quality()
-find_links(url, username, size)
+try:
+    print('use flickrit: %s' % flickrit_url)
+    get_links_flickit(username)
+except:
+    print('error flickrit, parse: %s' % url)
+    find_links(url, username, size)
